cd252cc78836e06f035f14e47de9923d22c8782a
vo01d <a.gurmana@gmail.com>
2024-08-17 15:10:43 +0300

    InputValidator and Main

---
+++ DataValidation/DataValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataValidation {
+    internal class InputValidator {
+        public static int Validate(string input) {
+            return int.Parse(input);
+        }
+        public static int Validate(string input, bool isPositive) {
+            int result = int.Parse(input);
+            if (result == 0) {
+                throw new ArgumentException("Input string is zero.");
+            } else if (isPositive && result < 0) {
+                throw new ArgumentException("Input string is negative integer.");
+            } else if (!isPositive && result > 0) {
+                throw new ArgumentException("Input string is positive integer.");
+            }
+
+            return result;
+        }
+        public static int Validate(string input, int rangeBegin, int rangeEnd) {
+            if (rangeBegin > rangeEnd) {
+                throw new ArgumentException("Range begin greater than range end.");
+            }
+
+            int result = int.Parse(input);
+            if (result < rangeBegin) {
+                throw new ArgumentException("Input string is below the range.");
+            }
+            if (result > rangeEnd) {
+                throw new ArgumentException("Input string is above the range.");
+            }
+
+            return result;
+        }
+    }
+}
